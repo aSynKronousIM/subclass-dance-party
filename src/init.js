3e29d3f45b868c9055e612d98553f4d68f8a37fb
Sean Kim <skim2636@gmail.com>
2015-02-11 11:29:24 -0800

    finished sprint

--- src/init.js
+++ src/init.js
@@ -18,16 +18,77 @@ $(document).ready(function(){
     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
 
     // get the maker function for the kind of dancer we're supposed to make
-    var dancerMakerFunction = window[dancerMakerFunctionName];
-
+    var dancerMakerFunction =  window[dancerMakerFunctionName];
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
-      $("body").height() * Math.random(),
+    var dancer = new dancerMakerFunction(
+      $("body").height() * ((Math.random() * 0.5) + 0.3),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
   });
+
+  $(".lineUpBottom").on("click", function(event){
+    $('breakDancer').stop();
+    var numDancers = $('.dancer').length;
+    $('.dancer').each(function(index, elem) {
+      $(elem).animate({ "top": "75%",
+        "left": Math.floor((index + 1)/(numDancers + 1) * screen.width) + "px"}, 2000);
+    });
+  });
+
+  $(".lineUpLeft").on("click", function(event){
+    $('breakDancer').stop();
+    $('.breakDancer').each(function(item, elem) {
+      cancelAnimationFrame(myReq);
+    });
+
+    var numDancers = $('.dancer').length;
+    $('.dancer').each(function(index, elem) {
+      $(elem).animate({ "top": Math.floor((index + 1)/(numDancers + 1) * screen.height) + "px",
+        "left": "15%"}, 2000);
+    });
+  });
+
+  var myReq;
+
+
+  $(".revolve").on("click", function() {
+    var $dancers = $('.breakDancer');
+    $dancers.each(function(index, elem) {
+    var position = $(elem).position();
+    var angle = 0, lastTime = null;
+    function animate(time) {
+      if (lastTime != null)
+        angle += (time - lastTime) * 0.001;
+      lastTime = time;
+      elem.style.top = (position.top + (Math.sin(angle) * 50)) + "px";
+      elem.style.left = (position.left + (Math.cos(angle) * 200)) + "px";
+      myReq = requestAnimationFrame(animate);
+      }
+      myReq = requestAnimationFrame(animate);
+    });
+  });
+
+  $('.free-dance').on('click', function() {
+    var $dancers = $('.dancer');
+    $dancers.each(function(index, elem) {
+      var topPos = $("body").height() * ((Math.random() * 0.5) + 0.3);
+      var leftPos = $("body").width() * Math.random();
+      $(elem).animate({"top": topPos, "left": leftPos}, 2000);
+      //Dancer.prototype.setPosition.call(elem, topPos, leftPos);
+    });
+  });
+
+  $('.dancer').filter('.balletDancer').on('mouseenter', function(event) {
+    $(this).css({
+      "-ms-transform": "rotate(180deg)",
+    "-webkit-transform": "rotate(180deg)",
+    "transform": "rotate(180deg)"
+    });
+  });
+
 });
+
 
--- src/modernDancer.js
+++ src/modernDancer.js
@@ -1,23 +1,18 @@
-var BlinkyDancer = function(top, left, timeBetweenSteps){
+var ModernDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps);
 };
 
-BlinkyDancer.prototype = Object.create(Dancer.prototype);
+ModernDancer.prototype = Object.create(Dancer.prototype);
 
 //update constructor
-BlinkyDancer.prototype.constructor = BlinkyDancer;
+ModernDancer.prototype.constructor = ModernDancer;
 
 //add new methods to makeBD.prototype
-BlinkyDancer.prototype.step = function(){  //remove param later
+ModernDancer.prototype.step = function(){  
   var self = this;
- 
   Dancer.prototype.step.call(self);
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
   self.$node.toggle();
 };
-
-
-
-
